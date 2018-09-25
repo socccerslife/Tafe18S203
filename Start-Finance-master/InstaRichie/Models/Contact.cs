da9924cd81ce523b6a24dce0121e00065fc023e5
Jaehong LEE <soccerslife@naver.com>
2018-09-26 08:51:17 +0930

    "Commit the branch feat_contact1.1"

---
+++ Start-Finance-master/InstaRichie/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ContactID { get; set; }
+        [NotNull]
+        public string FirstName { get; set; }
+        [NotNull]
+        public string LastName { get; set; }
+        [NotNull]
+        public string CompanyName { get; set; }
+        [Unique]
+        public string MobilePhone { get; set; }
+    }
+}
